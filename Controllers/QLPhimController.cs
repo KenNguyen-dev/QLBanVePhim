ec7eb9047705cd01dd81b0f75f0315417b74a476
Vantrieu2000 <75557478+Vantrieu2000@users.noreply.github.com>
2021-04-12 21:55:58 +0700

    Sửa thanh navbar và thêm trang QLPhim+AddPhim
    -Thanh navbar sửa lại cho cùng tone với trang bán vé
    - Thêm trang QLPhim
    -Thêm trang AddPhim

---
+++ Controllers/QLPhimController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLBanVePhim.Controllers
+{
+    public class QLPhimController : Controller
+    {
+        // GET: QLPhim
+        public ActionResult QLPhim()
+        {
+            return View();
+        }
+
+        public ActionResult AddPhim()
+        {
+            return View();
+        }
+    }
+}
